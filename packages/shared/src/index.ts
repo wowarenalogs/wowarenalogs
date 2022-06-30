96636542b57fa27524fc01509a372282f2ce8540
Paul Sauer <paulrsauer@gmail.com>
2022-06-29 23:05:31 -0400

    Re-add upload func

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,6 +3,7 @@ export { Button } from './components/common/Button';
 export { ClientContextProvider, useClientContext } from './hooks/ClientContext';
 export { AuthProvider } from './hooks/AuthContext';
 export { useAuth } from './hooks/AuthContext';
+export { uploadCombatAsync } from './utils/upload';
 export {
   initAnalyticsAsync,
   logAnalyticsEvent,
---
+++ packages/shared/src/utils/upload.ts
@@ -0,0 +1,36 @@
+import moment from 'moment-timezone';
+import { ICombatData } from '@wowarenalogs/parser';
+
+export async function uploadCombatAsync(
+  combat: ICombatData,
+  ownerId: string,
+  options?: {
+    patchRevision?: string;
+  },
+) {
+  const buffer = combat.rawLines.join('\n');
+
+  const headers: Record<string, string> = {
+    'content-type': 'text/plain;charset=UTF-8',
+    'x-goog-meta-wow-version': combat.wowVersion || 'shadowlands',
+    'x-goog-meta-ownerid': ownerId,
+    'x-goog-meta-starttime-utc': combat.startTime.toString(),
+
+    'x-goog-meta-client-timezone': moment.tz.guess(),
+    'x-goog-meta-client-year': new Date().getFullYear().toString(),
+  };
+
+  if (options?.patchRevision) {
+    headers['x-goog-meta-wow-patch-rev'] = options.patchRevision;
+  }
+
+  const storageSignerResponse = await fetch(`/api/getCombatUploadSignature/${combat.id}`, { headers });
+  const jsonResponse = await storageSignerResponse.json();
+  const signedUploadUrl = jsonResponse['url'];
+
+  return await fetch(signedUploadUrl, {
+    method: 'PUT',
+    body: buffer,
+    headers,
+  });
+}
