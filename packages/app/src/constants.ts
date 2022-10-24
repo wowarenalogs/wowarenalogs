6f72fc442746714802bbab0a2f1d4b25bae5513c
Kun Chen <kunchenguid@gmail.com>
2022-10-23 22:17:47 -0700

    fix preload script crash

--- packages/app/src/constants.ts
+++ packages/app/src/constants.ts
@@ -1,3 +1,3 @@
 import { app } from 'electron';
 
-export const BASE_REMOTE_URL = app.isPackaged ? 'https://desktop.wowarenalogs.com' : 'http://localhost:3000';
+export const BASE_REMOTE_URL = !app || app.isPackaged ? 'https://desktop.wowarenalogs.com' : 'http://localhost:3000';
