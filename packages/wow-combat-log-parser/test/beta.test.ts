c7f8eba76ac6121a608bc230b6bb91519737a338
Paul Sauer <paulrsauer@gmail.com>
2022-10-24 20:41:28 -0400

    adv log

--- packages/wow-combat-log-parser/test/beta.test.ts
+++ packages/wow-combat-log-parser/test/beta.test.ts
@@ -1,8 +1,9 @@
 import lineReader from 'line-reader';
 import path from 'path';
 
-import { ICombatData, WoWCombatLogParser } from '../src';
+import { CombatUnitClass, CombatUnitSpec, ICombatData, WoWCombatLogParser } from '../src';
 import { IMalformedCombatData } from '../src/CombatData';
+import { CombatResult } from '../src/types';
 
 const parseLogFileAsync = (logFileName: string): Promise<[ICombatData[], IMalformedCombatData[]]> => {
   return new Promise((resolve) => {
@@ -47,68 +48,69 @@ describe('parser tests', () => {
       expect(combats).toHaveLength(1);
     });
 
-    // it('reaction based fields should populate', () => {
-    //   expect(combats[0].playerTeamId).toEqual('0');
-    //   expect(combats[0].playerTeamRating).toEqual(1440);
-    // });
+    it('reaction based fields should populate', () => {
+      expect(combats[0].playerTeamId).toEqual('1');
+      expect(combats[0].playerTeamRating).toEqual(0);
+    });
 
-    // it('should buffer the raw log', () => {
-    //   expect(combats[0].rawLines.length).toEqual(12);
-    // });
+    it('should buffer the raw log', () => {
+      expect(combats[0].rawLines.length).toEqual(3641);
+    });
 
-    // it('should count the lines it cant parse', () => {
-    //   expect(combats[0].linesNotParsedCount).toEqual(1);
-    // });
+    it('should count the lines it cant parse', () => {
+      expect(combats[0].linesNotParsedCount).toEqual(386);
+    });
 
-    // it('should have correct combatant metadata', () => {
-    //   const combat = combats[0];
-    //   const combatant = combat.units['Player-57-0CE7FCBF'];
-    //   expect(combatant.class).toEqual(CombatUnitClass.Warrior);
-    //   expect(combatant.spec).toEqual(CombatUnitSpec.Warrior_Arms);
-    //   expect(combatant.info?.specId).toEqual(CombatUnitSpec.Warrior_Arms);
-    //   expect(combatant.info?.equipment[10].bonuses[2]).toEqual('1492');
-    //   expect(combatant.info?.teamId).toEqual('0');
-    //   expect(combatant.info?.highestPvpTier).toEqual(2);
-    //   expect(combatant.info?.covenantInfo.conduitIdsJSON).toEqual('[[169,184],[8,145]]');
-    //   expect(combatant.info?.covenantInfo.item3JSON).toEqual('[[1393],[1395],[1406],[1397]]');
-    // });
+    it('should have correct combatant metadata', () => {
+      const combat = combats[0];
+      const combatant = combat.units['Player-4184-00216AC8'];
+      expect(combatant.class).toEqual(CombatUnitClass.Shaman);
+      expect(combatant.spec).toEqual(CombatUnitSpec.Shaman_Restoration);
+      expect(combatant.info?.specId).toEqual(CombatUnitSpec.Shaman_Restoration);
 
-    // it('should parse arena start event', () => {
-    //   const combat = combats[0];
-    //   expect(combat.startInfo.timestamp).toBeGreaterThan(5000);
-    //   expect(combat.startInfo.zoneId).toEqual('1552');
-    //   expect(combat.startInfo.item1).toEqual('30');
-    //   expect(combat.startInfo.bracket).toEqual('2v2');
-    //   expect(combat.startInfo.isRanked).toEqual(true);
-    // });
+      const gloves = combatant.info?.equipment.find((i) => i.id === '153976');
+      expect(gloves).not.toBeNull();
 
-    // it('should parse arena end event', () => {
-    //   const combat = combats[0];
-    //   expect(combat.endInfo.timestamp).toBeGreaterThan(5000);
-    //   expect(combat.endInfo.matchDurationInSeconds).toEqual(465);
-    //   expect(combat.endInfo.winningTeamId).toEqual('1');
-    //   expect(combat.endInfo.team0MMR).toEqual(1440);
-    //   expect(combat.endInfo.team1MMR).toEqual(1437);
-    // });
+      expect(combatant.info?.teamId).toEqual('1');
+      expect(combatant.info?.highestPvpTier).toEqual(0);
+    });
 
-    // it('should have a correct death record', () => {
-    //   const combat = combats[0];
-    //   expect(combat.units['Player-57-0CE7FCBF']?.deathRecords).toHaveLength(1);
-    // });
+    it('should parse arena start event', () => {
+      const combat = combats[0];
+      expect(combat.startInfo.timestamp).toBeGreaterThan(5000);
+      expect(combat.startInfo.zoneId).toEqual('1504');
+      expect(combat.startInfo.item1).toEqual('34');
+      expect(combat.startInfo.bracket).toEqual('Skirmish');
+      expect(combat.startInfo.isRanked).toEqual(false);
+    });
 
-    // it('should be counted as a lost match', () => {
-    //   const combat = combats[0];
-    //   expect(combat.result).toEqual(CombatResult.Lose);
-    // });
+    it('should parse arena end event', () => {
+      const combat = combats[0];
+      expect(combat.endInfo.timestamp).toBeGreaterThan(5000);
+      expect(combat.endInfo.matchDurationInSeconds).toEqual(73);
+      expect(combat.endInfo.winningTeamId).toEqual('0');
+      expect(combat.endInfo.team0MMR).toEqual(0);
+      expect(combat.endInfo.team1MMR).toEqual(0);
+    });
 
-    // it('should have advanced logs parsed correctly', () => {
-    //   const combat = combats[0];
-    //   expect(combat.units['Player-57-0CE7FCBF']?.advancedActions).toHaveLength(1);
-    // });
+    it('should have a correct death record', () => {
+      const combat = combats[0];
+      expect(combat.units['Player-4184-00216AC8']?.deathRecords).toHaveLength(1);
+    });
 
-    // it('should compute the correct hash id', () => {
-    //   const combat = combats[0];
-    //   expect(combat.id).toEqual('65801fbacf7700cc1fa3744ecaffd4a2');
-    // });
+    it('should be counted as a lost match', () => {
+      const combat = combats[0];
+      expect(combat.result).toEqual(CombatResult.Lose);
+    });
+
+    it('should have advanced logs parsed correctly', () => {
+      const combat = combats[0];
+      expect(combat.units['Player-4184-00216AC8']?.advancedActions).toHaveLength(148);
+    });
+
+    it('should compute the correct hash id', () => {
+      const combat = combats[0];
+      expect(combat.id).toEqual('13f86bc508ebc6ab3e03f5272ea48333');
+    });
   });
 });
