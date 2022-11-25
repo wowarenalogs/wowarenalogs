ecd08f87c9cce6b8d1c4bd3d34e7b336f5ce716c
Paul Sauer <paulrsauer@gmail.com>
2022-11-25 13:32:42 -0500

    query colors

--- packages/shared/src/components/common/QueryError.tsx
+++ packages/shared/src/components/common/QueryError.tsx
@@ -5,7 +5,7 @@ export function QuerryError({ query }: { query: { error?: ApolloError } }) {
     if (query.error) {
       return (
         <div className="flex pt-4">
-          <div className="card w-full bg-primary text-primary-content">
+          <div className="card w-full bg-error text-error-content animate-fadein">
             <div className="card-body break-words">An error has occurred</div>
           </div>
         </div>
@@ -17,12 +17,12 @@ export function QuerryError({ query }: { query: { error?: ApolloError } }) {
   return (
     <div className="flex pt-4">
       {query.error?.graphQLErrors.map((e) => (
-        <div className="card w-full bg-primary text-primary-content" key={e.message}>
+        <div className="card w-full bg-error text-error-content animate-fadein" key={e.message}>
           <div className="card-body break-words">{e.message}</div>
         </div>
       ))}
       {query.error?.clientErrors.map((e) => (
-        <div className="card w-full bg-primary text-primary-content" key={e.message}>
+        <div className="card w-full bg-error text-error-content" key={e.message}>
           <div className="card-body break-words">{e.message}</div>
         </div>
       ))}
