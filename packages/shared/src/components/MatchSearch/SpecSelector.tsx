50a24b740676f8c34b872edad7000822bd7e17c3
Paul Sauer <paulrsauer@gmail.com>
2022-11-24 20:21:26 -0500

    tweaks to spec picker

--- packages/shared/src/components/MatchSearch/SpecSelector.tsx
+++ packages/shared/src/components/MatchSearch/SpecSelector.tsx
@@ -1,5 +1,5 @@
 import { CombatUnitSpec } from '@wowarenalogs/parser';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { SpecImage } from '../common/SpecImage';
 
@@ -11,6 +11,7 @@ const SPECS = Object.entries(CombatUnitSpec)
     CLASS_NAMES.add(parts[0]);
     return [parts[0], parts[1], c[1]];
   });
+const SPEC_BY_CLASS = Array.from(CLASS_NAMES).map((c) => SPECS.filter((s) => s[0] === c));
 
 export function SpecSelector({
   spec,
@@ -26,17 +27,18 @@ export function SpecSelector({
   if (spec) {
     return (
       <div
-        className="inline-block w-[48px] h-[48px] mr-2 border-gray-400 border rounded"
+        className="inline-block w-[48px] h-[48px] mr-2 border-gray-400 border rounded hover:opacity-20 hover:border-red-600 hover:border-2"
         onClick={() => removeCallback(spec)}
       >
         <SpecImage specId={spec} size={48} />
       </div>
     );
   }
+
   return (
     <div className="dropdown w-[48px] h-[48px] mr-2">
       <label
-        onClick={(e) => {
+        onClick={() => {
           if (isShowing) {
             if (document.activeElement instanceof HTMLElement) {
               document.activeElement.blur();
@@ -45,37 +47,38 @@ export function SpecSelector({
           setIsShowing(!isShowing);
         }}
         tabIndex={0}
+        onBlur={() => {
+          setIsShowing(false);
+        }}
         className="btn w-[48px] border-gray-400 border rounded"
       >
         ?
       </label>
-      <div tabIndex={0} className="dropdown-content div p-2 shadow bg-gray-900 rounded-box w-[124px]">
-        {SPECS.map((s) => {
+      <div
+        tabIndex={0}
+        className="flex flex-wrap space-y-1 dropdown-content div p-2 shadow bg-gray-900 rounded-box w-[414px]"
+      >
+        {SPEC_BY_CLASS.map((s) => {
           return (
-            <div
-              className="inline-block"
-              key={s[2]}
-              onClick={(e) => {
-                addCallback(s[2] as CombatUnitSpec);
-              }}
-            >
-              <SpecImage specId={s[2]} size={36} />
+            <div className="mr-4" key={s[0][0]}>
+              <div>{s[0][0]}</div>
+              <div className="flex flex-row space-x-1">
+                {s.map((a) => (
+                  <div
+                    className="hover:border-gray-600 rounded border-2 border-transparent box-border transition-colors duration-150"
+                    key={a[2]}
+                    onClick={(e) => {
+                      addCallback(a[2] as CombatUnitSpec);
+                    }}
+                  >
+                    <SpecImage specId={a[2]} size={36} />
+                  </div>
+                ))}
+              </div>
             </div>
           );
         })}
       </div>
     </div>
   );
-  // return (
-  //   <select className="select w-full max-w-xs" onChange={(e) => props.addCallback(e.target.value as CombatUnitSpec)}>
-  //     <option disabled selected>
-  //       spec
-  //     </option>
-  //     {SPECS.map((s) => (
-  //       <option key={s[2]} value={s[2]}>
-  //         <SpecImage specId={s[2]} />
-  //       </option>
-  //     ))}
-  //   </select>
-  // );
 }
