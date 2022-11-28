e055654cab11e1d96f15d78f2f56968926a83c12
Kun Chen <kunchenguid@gmail.com>
2022-11-28 12:32:52 -0800

    put up download page in web

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  presets: [require('../shared/tailwind.config')],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '../shared/src/components/**/*.{js,ts,jsx,tsx}',
+  ],
 };
