1f9dcfd6d37a1b5eaf7b5dca77e751250a823ac7
Paul Sauer <paulrsauer@gmail.com>
2022-12-16 14:19:15 -0500

    poc

--- packages/cloud/src/refreshSpellIconsHandler.ts
+++ packages/cloud/src/refreshSpellIconsHandler.ts
@@ -1,8 +1,6 @@
 import { Firestore } from '@google-cloud/firestore';
 import { Storage as GoogleCloudStorage } from '@google-cloud/storage';
-import { writeFileSync } from 'fs-extra';
 import _ from 'lodash';
-import path from 'path';
 import superagent from 'superagent';
 
 import { AtomicArenaCombat } from '../../parser/dist/index';
@@ -51,14 +49,28 @@ const processSpellIdAsync = async (spellId: string): Promise<boolean> => {
     return false;
   }
 
-  const writeFilePath = path.join(__dirname, `${spellId}.jpg`);
-  writeFileSync(writeFilePath, imgResponse.body);
-  await bucket.upload(writeFilePath, {
-    destination: `spells/${spellId}.jpg`,
-    contentType: 'image/jpeg',
-  });
+  const fileWritten = await writeToBucket(`spells/${spellId}.jpg`, imgResponse.body);
+  if (!fileWritten) {
+    console.log(`Unable to write ${spellId}`);
+  }
   return true;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function writeToBucket(fileName: string, fileData: any) {
+  return new Promise((res, rej) => {
+    bucket
+      .file(fileName)
+      .createWriteStream()
+      .write(fileData, (error) => {
+        if (error) {
+          rej(error);
+        } else {
+          res(true);
+        }
+      });
+  });
+}
 
 export async function handler(_event: unknown, _context: unknown, callback: () => void) {
   console.log('refreshSpellIconsHandler started');
