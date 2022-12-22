5f9b5c0bde6209e92a94d25daac742030e4ea372
Paul Sauer <paulrsauer@gmail.com>
2022-12-22 12:05:39 -0500

    Simple log view for debugging

---
+++ packages/shared/src/components/CombatReport/CombatLogView/index.tsx
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import { useState } from 'react';
+
+import { useCombatReportContext } from '../CombatReportContext';
+
+const lowerIncludes = (e: string, x: string) => {
+  return e.toLowerCase().includes(x.toLowerCase());
+};
+
+export const CombatLogView = () => {
+  const { combat } = useCombatReportContext();
+  const [textFilter, setTextFilter] = useState('');
+
+  if (!combat) return <div>Could not read combat</div>;
+
+  const lines = combat.rawLines.filter((e) => lowerIncludes(e, textFilter));
+  const debouncedUpdate = _.debounce(setTextFilter, 300);
+
+  return (
+    <div>
+      <input
+        placeholder="search log..."
+        className="input input-bordered w-full max-w-xs"
+        onChange={(evt) => {
+          debouncedUpdate(evt.target.value);
+        }}
+      />
+      <div className="text-xs mt-2">
+        {lines.map((e, i) => (
+          <div key={i}>{e}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
