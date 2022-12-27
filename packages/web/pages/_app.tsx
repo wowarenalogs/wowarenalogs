ffb251fff4b9d755121005b3cd34af7024e72d76
Kun Chen <kunchenguid@gmail.com>
2022-12-26 16:06:59 -0800

    setup google analytics on web

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -2,8 +2,14 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
+import WebLayout from '../components/WebLayout';
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WebLayout>
+      <Component {...pageProps} />
+    </WebLayout>
+  );
 }
 
 export default App;
