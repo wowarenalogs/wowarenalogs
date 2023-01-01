46402586e2ebb12cabf774c5bf80b89482088272
Paul Sauer <paulrsauer@gmail.com>
2023-01-01 14:57:30 -0500

    remove anonymizer

--- packages/cloud/index.js
+++ packages/cloud/index.js
@@ -1,7 +1,5 @@
 'use strict';
 var writeMatchStubHandler = require('./dist/cloud/src/writeMatchStubHandler.js');
-var writeAnonLogHandler = require('./dist/cloud/src/writeAnonLogHandler.js');
 var refreshSpellIconsHandler = require('./dist/cloud/src/refreshSpellIconsHandler.js');
 module.exports.writeMatchStubHandler = writeMatchStubHandler.handler;
-module.exports.writeAnonLogHandler = writeAnonLogHandler.handler;
 module.exports.refreshSpellIconsHandler = refreshSpellIconsHandler.handler;
