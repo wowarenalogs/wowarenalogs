2a0347d0df2791589795e8e872756ffdd3754c25
Kyle Montag <KMontag42@users.noreply.github.com>
2023-01-04 15:16:16 -0500

    Update spell data with new prevoker spells

--- packages/shared/src/data/awcSpells.ts
+++ packages/shared/src/data/awcSpells.ts
@@ -170,11 +170,11 @@ export const awcSpells: Record<CombatUnitSpec, string[]> = {
 
   //   Evoker_Preservation = "1468",
   '1468': [
+    '357170', // time dilation
+    '363534', // rewind
     '363916', // obsidian scales
     '370553', // tip the scales
-    '374348', // renewing blaze
-    '363534', // rewind
-    '357170', // time dilation
     '370960', // emerald communion
+    '374348', // renewing blaze
   ],
 };
