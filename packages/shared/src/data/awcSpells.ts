85d387c79863a409f593e70f166de7cc0b5faddf
Kyle Montag <KMontag42@users.noreply.github.com>
2023-01-04 12:48:22 -0500

    add presevation evoker spell ids to awsSpells

--- packages/shared/src/data/awcSpells.ts
+++ packages/shared/src/data/awcSpells.ts
@@ -169,5 +169,12 @@ export const awcSpells: Record<CombatUnitSpec, string[]> = {
   '1467': [],
 
   //   Evoker_Preservation = "1468",
-  '1468': [],
+  '1468': [
+    '363916', // obsidian scales
+    '370553', // tip the scales
+    '374348', // renewing blaze
+    '363534', // rewind
+    '357170', // time dilation
+    '370960', // emerald communion
+  ],
 };
