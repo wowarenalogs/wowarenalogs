7c466cc12dbccce9bea8669d8917d67d3b11c76f
kunchenguid <kunchenguid@gmail.com>
2023-01-28 12:01:47 -0800

    merge damage from same spell on timeline

--- packages/shared/src/components/CombatReport/CombatUnitTimelineView/CombatUnitHpUpdate.tsx
+++ packages/shared/src/components/CombatReport/CombatUnitTimelineView/CombatUnitHpUpdate.tsx
@@ -5,12 +5,20 @@ import {
   getClassColor,
   ICombatUnit,
 } from '@wowarenalogs/parser';
+import _ from 'lodash';
 import Image from 'next/image';
+import { useMemo } from 'react';
 
 import { Utils } from '../../../utils/utils';
 
 interface IProps {
-  action: CombatHpUpdateAction;
+  actionGroup: {
+    srcUnitId: string;
+    destUnitId: string;
+    spellId: string;
+    spellName: string;
+    actions: CombatHpUpdateAction[];
+  };
   unit: ICombatUnit;
   combat: AtomicArenaCombat;
   groupTotal: number;
@@ -19,20 +27,54 @@ interface IProps {
 
 export const CombatUnitHpUpdate = (props: IProps) => {
   const colorSourceUnitId =
-    props.action.destUnitId === props.unit.id ? props.action.srcUnitId : props.action.destUnitId;
+    props.actionGroup.destUnitId === props.unit.id ? props.actionGroup.srcUnitId : props.actionGroup.destUnitId;
   const colorSourceUnit = props.combat.units[colorSourceUnitId];
   const colorSourceUnitClass = colorSourceUnit ? colorSourceUnit.class : CombatUnitClass.None;
+  const totalEffectiveAmount = useMemo(
+    () => _.sum(props.actionGroup.actions.map((a) => a.effectiveAmount)),
+    [props.actionGroup.actions],
+  );
+  const hasCrit = useMemo(() => props.actionGroup.actions.some((a) => a.isCritical), [props.actionGroup.actions]);
+  const tooltip = useMemo(() => {
+    const spellName = props.actionGroup.spellName || 'Auto Attack';
+    if (props.actionGroup.actions.length === 1) {
+      return `${spellName}: ${Utils.printCombatNumber(Math.abs(totalEffectiveAmount), hasCrit)}`;
+    }
 
-  const widthPercentage = (Math.abs(props.action.effectiveAmount) / props.groupTotal) * 100;
-  const widthPercentageAbsolute = (Math.abs(props.action.effectiveAmount) / props.timelineMax) * 100;
+    const normalHits = props.actionGroup.actions.filter((a) => !a.isCritical);
+    const critHits = props.actionGroup.actions.filter((a) => a.isCritical);
+
+    const normalHitsText =
+      normalHits.length > 0
+        ? `
+
+${normalHits.length} normal hits x ${Utils.printCombatNumber(
+            _.sum(normalHits.map((a) => Math.abs(a.effectiveAmount))) / normalHits.length,
+          )} per hit = ${Utils.printCombatNumber(_.sum(normalHits.map((a) => Math.abs(a.effectiveAmount))))} total`
+        : '';
+
+    const critHitsText =
+      critHits.length > 0
+        ? `
+
+${critHits.length} crit hits x ${Utils.printCombatNumber(
+            _.sum(critHits.map((a) => Math.abs(a.effectiveAmount))) / critHits.length,
+          )} per hit = ${Utils.printCombatNumber(_.sum(critHits.map((a) => Math.abs(a.effectiveAmount))))} total`
+        : '';
+
+    return `${spellName}: ${Utils.printCombatNumber(
+      Math.abs(totalEffectiveAmount),
+      hasCrit,
+    )}${normalHitsText}${critHitsText}`;
+  }, [props.actionGroup.actions, props.actionGroup.spellName, totalEffectiveAmount, hasCrit]);
+
+  const widthPercentage = (Math.abs(totalEffectiveAmount) / props.groupTotal) * 100;
+  const widthPercentageAbsolute = (Math.abs(totalEffectiveAmount) / props.timelineMax) * 100;
 
   return (
     <div
-      className="tooltip flex flex-row"
-      data-tip={`${props.action.spellName || 'Auto Attack'}: ${Utils.printCombatNumber(
-        Math.abs(props.action.effectiveAmount),
-        props.action.isCritical,
-      )}`}
+      className="tooltip flex flex-row whitespace-pre-line text-left z-50"
+      data-tip={tooltip}
       style={{
         minWidth: '4px',
         width: widthPercentage.toFixed(2) + '%',
@@ -44,20 +86,20 @@ export const CombatUnitHpUpdate = (props: IProps) => {
           backgroundColor: getClassColor(colorSourceUnitClass),
         }}
       >
-        {widthPercentageAbsolute >= 10 && props.action.spellId ? (
+        {widthPercentageAbsolute >= 10 && props.actionGroup.spellId ? (
           <div className="ml-1 w-4 h-4" style={{ minWidth: 16, minHeight: 16 }}>
             <Image
               className="rounded"
-              src={Utils.getSpellIcon(props.action.spellId) ?? 'https://images.wowarenalogs.com/spells/0.jpg'}
+              src={Utils.getSpellIcon(props.actionGroup.spellId) ?? 'https://images.wowarenalogs.com/spells/0.jpg'}
               width={16}
               height={16}
-              alt={props.action.spellName ?? ''}
+              alt={props.actionGroup.spellName ?? ''}
             />
           </div>
         ) : null}
-        {widthPercentageAbsolute >= 30 && props.action.spellId ? (
+        {widthPercentageAbsolute >= 30 && props.actionGroup.spellId ? (
           <div className="ml-1 text-black font-medium text-xs">
-            {Utils.printCombatNumber(Math.abs(props.action.effectiveAmount), props.action.isCritical)}
+            {Utils.printCombatNumber(Math.abs(totalEffectiveAmount), hasCrit)}
           </div>
         ) : null}
       </div>
