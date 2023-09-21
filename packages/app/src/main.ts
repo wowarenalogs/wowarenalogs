6d002dbbc08a341e39fcc4d2f84c1bb43ced9360
Kun Chen <kunchenguid@gmail.com>
2023-09-20 22:37:35 -0700

    fix second-instance behavior

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 import { autoUpdater } from 'electron-updater';
 import moment from 'moment';
@@ -44,16 +44,47 @@ function createWindow() {
   });
 
   nativeBridgeRegistry.startListeners(win);
+
+  return win;
 }
 
-app.on('ready', () => {
-  createWindow();
+const isFirstInstance = app.requestSingleInstanceLock();
 
-  if (app.isPackaged) {
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-});
-
-app.on('window-all-closed', () => {
+if (!isFirstInstance) {
   app.quit();
-});
+} else {
+  app.on('ready', () => {
+    const win = createWindow();
+
+    if (app.isPackaged) {
+      autoUpdater.checkForUpdatesAndNotify().then((result) => {
+        if (result?.updateInfo.version === app.getVersion()) {
+          dialog
+            .showMessageBox(win, {
+              type: 'question',
+              buttons: ['Update Now', 'Skip'],
+              defaultId: 0,
+              title: 'Update Available',
+              message: 'A new version of the app is available. Would you like to update now?',
+            })
+            .then((response) => {
+              if (response.response === 0) {
+                autoUpdater.quitAndInstall();
+              }
+            });
+        }
+      });
+    }
+
+    app.on('second-instance', () => {
+      if (!win.isVisible()) {
+        win.show();
+      }
+      win.focus();
+    });
+  });
+
+  app.on('window-all-closed', () => {
+    app.quit();
+  });
+}
