11a6641dd4b67981d712e9ac158131ecb5ec44ab
Paul Sauer <paulrsauer@gmail.com>
2023-10-13 03:19:17 -0400

    rm now unused call

--- packages/app/src/nativeBridge/registry.ts
+++ packages/app/src/nativeBridge/registry.ts
@@ -110,43 +110,6 @@ export class NativeBridgeRegistry {
     return typeString;
   }
 
-  public generateAPIObject(): Object {
-    return Object.assign(
-      {},
-      ...this.modules.map((module) => {
-        const ctor = Object.getPrototypeOf(module).constructor;
-        const moduleMetadata = MODULE_METADATA.get(ctor);
-        if (!moduleMetadata) {
-          throw new Error('module metadata not found');
-        }
-
-        const moduleApi: Record<string, Object> = {};
-
-        Object.values(moduleMetadata.functions).forEach((func) => {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          moduleApi[func.name] = (...args: any[]) => {
-            return ipcRenderer.invoke(getModuleFunctionKey(moduleMetadata.name, func.name), ...args);
-          };
-        });
-
-        Object.values(moduleMetadata.events).forEach((evt) => {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          moduleApi[evt.name] = (callback: (event: IpcRendererEvent, ...args: any[]) => void) =>
-            evt.type === 'on'
-              ? ipcRenderer.on(getModuleEventKey(moduleMetadata.name, evt.name), callback)
-              : ipcRenderer.once(getModuleEventKey(moduleMetadata.name, evt.name), callback);
-
-          moduleApi[`removeAll_${evt.name}_listeners`] = () =>
-            ipcRenderer.removeAllListeners(getModuleEventKey(moduleMetadata.name, evt.name));
-        });
-
-        return {
-          [moduleMetadata.name]: moduleApi,
-        };
-      }),
-    );
-  }
-
   public startListeners(mainWindow: BrowserWindow): void {
     this.modules.forEach((module) => {
       const ctor = Object.getPrototypeOf(module).constructor;
