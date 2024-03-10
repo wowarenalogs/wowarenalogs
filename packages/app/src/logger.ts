85496a17a233822a5baea3a07b6044d9299a46c4
Paul Sauer <paulrsauer@gmail.com>
2024-03-10 15:39:01 -0400

    Fix test logic

--- packages/app/src/logger.ts
+++ packages/app/src/logger.ts
@@ -3,7 +3,7 @@ import { app } from 'electron';
 import { createWriteStream } from 'fs-extra';
 
 let logConsole = console;
-if (app && !app.isPackaged) {
+if (app && app.isPackaged) {
   // app will be undefined during preload compile
   const output = createWriteStream('./log.txt');
   logConsole = new Console(output, output);
