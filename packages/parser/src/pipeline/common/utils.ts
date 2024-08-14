5eb3a1f14d807223c46c2a1930e9262990ce8957
Paul Sauer <paulrsauer@gmail.com>
2024-08-13 22:01:21 -0400

    Fix moment timezone ctor

--- packages/parser/src/pipeline/common/utils.ts
+++ packages/parser/src/pipeline/common/utils.ts
@@ -1,35 +1,3 @@
-import moment from 'moment-timezone';
-
 export function isNonNull<T>(value: T): value is NonNullable<T> {
   return value != null;
-}
-
-export function getTimestamp(
-  month: number,
-  day: number,
-  year: number,
-  hour: number,
-  minute: number,
-  second: number,
-  ms: number,
-  timezone?: string,
-) {
-  const intTZOffset = timezone ? parseInt(timezone) : 0;
-
-  return moment
-    .tz(
-      {
-        ms,
-        M: month - 1,
-        d: day,
-        h: hour,
-        m: minute,
-        s: second,
-        y: year,
-      },
-      'UTC',
-    )
-    .utcOffset(intTZOffset)
-    .utc()
-    .valueOf();
 }
