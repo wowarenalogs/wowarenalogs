d986955cef54c28bbef8fd852057a0f0ea7c8561
Paul Sauer <paulrsauer@gmail.com>
2024-08-16 20:40:23 -0400

    Always fun to debug the bit twiddling in their lura

--- packages/shared/src/components/CombatReport/CombatPlayers/talentStrings.ts
+++ packages/shared/src/components/CombatReport/CombatPlayers/talentStrings.ts
@@ -2,6 +2,17 @@ import talentIdMapData from '../../../data/talentIdMap.json';
 
 export type RaidBotsTalentData = RaidbotsTalentSpec[];
 
+// The current version is defined by C_Traits.GetLoadoutSerializationVersion()
+const SERIALIZATION_VERSION = 2;
+
+// local BitsPerChar = 6;
+const BitsPerChar = 6;
+
+// ClassTalentImportExportMixin.bitWidthHeaderVersion = 8;
+// ClassTalentImportExportMixin.bitWidthSpecID = 16;
+// ClassTalentImportExportMixin.bitWidthRanksPurchased = 6;
+const bitWidthRanksPurchased = 6;
+
 export interface RaidbotsTalentSpec {
   traitTreeId: number;
   className: string;
@@ -9,6 +20,8 @@ export interface RaidbotsTalentSpec {
   specName: string;
   specId: number;
   classNodes: ClassNode[];
+  heroNodes: HeroNode[];
+  subTreeNodes: SubtreeNode[];
   specNodes: SpecNode[];
   fullNodeOrder: number[];
 }
@@ -28,6 +41,34 @@ export interface ClassNode {
   reqPoints?: number;
 }
 
+export interface HeroNode {
+  id: number;
+  name: string;
+  type: string;
+  posX: number;
+  posY: number;
+  maxRanks: number;
+  entryNode?: boolean;
+  subTreeId: number;
+  requiresNode: number;
+  next: number[];
+  prev: number[];
+  entries: Entry[];
+  freeNode?: boolean;
+}
+
+export interface SubtreeNode {
+  id: number;
+  name: string;
+  type: string;
+  posX: number;
+  posY: number;
+  entryNode?: boolean;
+  next: number[];
+  prev: number[];
+  entries: SubtreeNodeEntry[];
+}
+
 export interface Entry {
   id: number;
   definitionId: number;
@@ -37,6 +78,16 @@ export interface Entry {
   spellId: number;
   icon: string;
   index: number;
+}
+
+export interface SubtreeNodeEntry {
+  id: number;
+  type: string;
+  name: string;
+  traitSubTreeId: number;
+  traitTreeId: number;
+  atlasMemberName: string;
+  nodes: number[];
 }
 
 export interface SpecNode {
@@ -80,11 +131,12 @@ const emptyTreeHash: ExportStream = Array(128 / 8).fill({
 type MappedRaidbotsSpec = RaidbotsTalentSpec & {
   specNodeMap: Record<number, SpecNode>;
   classNodeMap: Record<number, ClassNode>;
-  // subtreeNodeMap: Record<number, Entry>;
-  // heroNodeMap: Record<number, Entry>;
+  subtreeNodeMap: Record<number, SubtreeNode>;
+  heroNodeMap: Record<number, HeroNode>;
 };
 
 export const nodeMaps: Record<number, MappedRaidbotsSpec> = {};
+const fullNodeMapping: Record<number, SpecNode | ClassNode | HeroNode | SubtreeNode> = {};
 
 talentIdMap.forEach((spec) => {
   nodeMaps[spec.specId] = {
@@ -92,6 +144,7 @@ talentIdMap.forEach((spec) => {
     classNodeMap: spec.classNodes.reduce(
       (prev, cur) => {
         prev[cur.id] = cur;
+        fullNodeMapping[cur.id] = cur;
         return prev;
       },
       {} as Record<number, ClassNode>,
@@ -99,16 +152,49 @@ talentIdMap.forEach((spec) => {
     specNodeMap: spec.specNodes.reduce(
       (prev, cur) => {
         prev[cur.id] = cur;
+        fullNodeMapping[cur.id] = cur;
         return prev;
       },
       {} as Record<number, SpecNode>,
     ),
+    heroNodeMap: spec.heroNodes.reduce(
+      (prev, cur) => {
+        prev[cur.id] = cur;
+        fullNodeMapping[cur.id] = cur;
+        return prev;
+      },
+      {} as Record<number, HeroNode>,
+    ),
+    subtreeNodeMap: spec.subTreeNodes.reduce(
+      (prev, cur) => {
+        prev[cur.id] = cur;
+        fullNodeMapping[cur.id] = cur;
+        return prev;
+      },
+      {} as Record<number, SubtreeNode>,
+    ),
   };
 });
 
-const BitsPerChar = 6;
-const bitWidthRanksPurchased = 6;
+// local function MakeBase64ConversionTable()
+// 	local base64ConversionTable = {};
+// 	base64ConversionTable[0] = 'A';
+// 	for num = 1, 25 do
+// 		table.insert(base64ConversionTable, string.char(65 + num));
+// 	end
 
+// 	for num = 0, 25 do
+// 		table.insert(base64ConversionTable, string.char(97 + num));
+// 	end
+
+// 	for num = 0, 9 do
+// 		table.insert(base64ConversionTable, tostring(num));
+// 	end
+
+// 	table.insert(base64ConversionTable, '+');
+// 	table.insert(base64ConversionTable, '/');
+// 	return base64ConversionTable;
+// end
 const b64Table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
 function wowExportTo64(dataEntries: ExportStream) {
@@ -159,10 +245,11 @@ function addValue(exportStream: ExportStream, bitWidth: number, value: number) {
 }
 
 function writeLoadoutContent(
-  treeNodes: (ClassNode | SpecNode)[],
+  treeNodes: (ClassNode | SpecNode | HeroNode | SubtreeNode)[],
   talentsPicked: { id1: number; id2: number; count: number }[],
 ) {
   const exportStream: ExportStream = [];
+
   for (let i = 0; i < treeNodes.length; i++) {
     const treeNode = treeNodes[i];
 
@@ -172,19 +259,29 @@ function writeLoadoutContent(
     }
 
     const talentSelection = talentsPicked.find((i) => i.id1 === treeNode?.id);
+    // debug missing nodes, should never happen (lol)
+    // if (!talentSelection) {
+    //   console.log(treeNode);
+    // }
 
     const isNodeSelected = talentSelection !== undefined;
 
-    const isPartiallyRanked = talentSelection && talentSelection.count < treeNode.maxRanks;
-    const isChoiceNode = treeNode?.type === 'choice';
+    const isPartiallyRanked = 'maxRanks' in treeNode && talentSelection && talentSelection.count < treeNode.maxRanks;
+    const isChoiceNode = treeNode?.type === 'choice' || treeNode?.type === 'subtree';
 
-    if (treeNode.freeNode) {
+    if ('freeNode' in treeNode && treeNode.freeNode) {
       addValue(exportStream, 1, 0);
       continue;
     }
+
     addValue(exportStream, 1, isNodeSelected ? 1 : 0);
 
     if (isNodeSelected) {
+      // This extra addValue is in bliz code, I'm not sure why it's needed
+      // their code makes some differentiation between a talent node being "selected" vs. "purchased"
+      // but I don't see any differentiation in the game client or the combat log data :thinking:
+      addValue(exportStream, 1, isNodeSelected ? 1 : 0);
+
       addValue(exportStream, 1, isPartiallyRanked ? 1 : 0);
 
       if (isPartiallyRanked) {
@@ -195,6 +292,7 @@ function writeLoadoutContent(
 
       if (isChoiceNode) {
         const entryIndex = treeNode.entries.findIndex((t) => t.id === talentSelection?.id2); // GET ACTIVE ENTRY TODO
+        // console.log('choice index', entryIndex);
         if (entryIndex <= 0 || entryIndex > 4) {
           // error("Error exporting tree node " .. treeNode.ID .. ". The active choice node entry index (" .. entryIndex .. ") is out of bounds. ");
         }
@@ -209,14 +307,18 @@ function writeLoadoutContent(
 export const createExportString = (specId: number, talents: { id1: number; id2: number; count: number }[]) => {
   const specData = nodeMaps[specId];
 
-  const treeNodes: (ClassNode | SpecNode)[] = specData.fullNodeOrder.map((n) => {
-    return specData.specNodeMap[n] || specData.classNodeMap[n];
+  const treeNodes = specData.fullNodeOrder.map((n) => {
+    return fullNodeMapping[n];
   });
+
+  // console.log(`Empty tree nodes count: ${treeNodes.filter((n) => !n).length}`);
+  // console.log(`Number of talents: ${talents.length}`);
+  // console.log({ treeNodes }, specData.fullNodeOrder);
 
   const loadout = writeLoadoutContent(treeNodes, talents);
   return wowExportTo64([
     {
-      value: 1,
+      value: SERIALIZATION_VERSION,
       bitWidth: 8,
     },
     {
