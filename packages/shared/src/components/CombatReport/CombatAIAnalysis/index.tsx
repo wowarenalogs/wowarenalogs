e305f074d4b08158728bd3d60f89027a05e2c525
Mingjian Liu <mingjianliu@macbookpro.lan>
2026-04-24 00:58:01 -0700

    feat(F66): pass outgoingCCChains into buildMatchTimeline call

--- packages/shared/src/components/CombatReport/CombatAIAnalysis/index.tsx
+++ packages/shared/src/components/CombatReport/CombatAIAnalysis/index.tsx
@@ -238,6 +238,7 @@ export function buildMatchContext(
         isHealer: healer,
         playerIdMap,
         enemyIdMap,
+        outgoingCCChains,
       } as BuildMatchTimelineParams),
     );
 
